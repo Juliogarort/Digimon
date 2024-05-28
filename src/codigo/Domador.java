9fd770182c26cd5332dddd55eac4231988de5fef
JulioGarcía <julio.garort16@gmail.com>
2024-05-28 20:34:28 +0200

    declaración atributos en clase domador con nombre de los digimon guardados en lista

--- src/codigo/Domador.java
+++ src/codigo/Domador.java
@@ -1,5 +1,24 @@
 package codigo;
 
-public class Domador {
+import java.util.*;
 
+public class Domador {
+    private String nombre;
+    private List<Digimon> equipo;
+
+    public Domador(String nombre) {
+        this.nombre = nombre;
+        this.equipo = new ArrayList<>(3);
+        equipo.add(new Digimon("Agumon"));
+        equipo.add(new Digimon("Gabumon"));
+        equipo.add(new Digimon("Patamon"));
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public List<Digimon> getEquipo() {
+        return equipo;
+    }
 }
