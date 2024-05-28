e038511db0c3cf36db44be148e53aefab59752e6
JulioGarcía <julio.garort16@gmail.com>
2024-05-28 20:19:25 +0200

    Pedir nombre del domador en main y creación de constructor y metodos de la clase Digimon

--- src/codigo/BatallaDigital.java
+++ src/codigo/BatallaDigital.java
@@ -1,5 +1,17 @@
 package codigo;
 
+import java.util.Scanner;
+
 public class BatallaDigital {
 
+	   public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+
+	        System.out.println("Ingrese el nombre del domador:");
+	        String nombreDomador = scanner.nextLine();
+	        Domador domador = new Domador(nombreDomador);
+
+
+	    }
+	
 }
